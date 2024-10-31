5e18bcc451c84caf154f1592ab5760863a27fc6f
unknown <franhe345@gmail.com>
2024-10-30 21:05:39 -0300

    agregue entidades deporte y categoria. creacion de menu

--- tp-final/proyecto-4/Deporte.cs
+++ tp-final/proyecto-4/Deporte.cs
@@ -1,19 +1,52 @@
 ﻿using System;
+using System.Collections;
 
 namespace proyecto_4
 {
 	public class Deporte
 	{
-		public Deporte()
-		{
 //			Atributos
-			
+		private string nombre;
+		private ArrayList listaCategorias;
+		private ArrayList listadoEntrenadores;
+		
 //			Constructor
-			
+		public Deporte(string nombre)
+		{
+			this.nombre = nombre;
+		}
+		
 //			Propiedades
-			
+		public string Nombre
+		{
+			get { return nombre; }
+			set { nombre = value; }
+		}
+		
+		public ArrayList ListaCategorias
+		{
+			get { return listaCategorias; }
+		}
+		
+		public ArrayList ListadoEntrenadores
+		{
+			get { return listadoEntrenadores; }
+		}
+		
 //			Metodos
-			
+		public void listadoDeporte()
+		{
+//				TO-DO
+		}
+		
+		public void listadoDeporteCategoria()
+		{
+//				TO-DO
+		}
+		
+		public void listadoTotal()
+		{
+//				TO-DO
 		}
 	}
 }
--- tp-final/proyecto-4/Program.cs
+++ tp-final/proyecto-4/Program.cs
@@ -1,12 +1,4 @@
-﻿/*
- * Created by SharpDevelop.
- * User: fraan
- * Date: 10/22/2024
- * Time: 12:27 AM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 
 namespace proyecto_4
 {
@@ -14,12 +6,56 @@ namespace proyecto_4
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			bool salir = false;
+			while(!salir)
+			{
+				Console.WriteLine("-----");
+				Console.WriteLine("MENÚ");
+				Console.WriteLine("-----------------------------------------------");
+				Console.WriteLine("1. Dar de alta a un Entrenador");
+				Console.WriteLine("2. Dar de baja a un Entrenador");
+				Console.WriteLine("3. Dar de alta a un Niño/Socio en un Deporte");
+				Console.WriteLine("4. Dar de baja a un Niño/Socio en un Deporte");
+				Console.WriteLine("5. Simular el pago de una cuota");
+				Console.WriteLine("6. Submenu");
+				Console.WriteLine("7. Listado de Deudores");
+				Console.WriteLine("8. Agregar un Deporte");
+				Console.WriteLine("9. Eliminar un Deporte");
+				Console.WriteLine("10. Salir");
+				Console.WriteLine("-----------------------------------------------");
+				
+				Console.Write("Seleccione una opción: ");
+				int opcion = int.Parse(Console.ReadLine());
+				Console.Clear();
+				
+				switch(opcion)
+				{
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+					case 6:
+						break;
+					case 7:
+						break;
+					case 8:
+						break;
+					case 9:
+						break;
+					case 10:
+						salir = true;
+						break;
+					default:
+						Console.WriteLine("Opcion no valida. Por favor, ingrese una opcion valida.");
+						break;
+				}
+			}
 		}
 	}
 }
